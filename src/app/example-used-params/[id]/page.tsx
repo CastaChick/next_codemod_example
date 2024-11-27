7eccadb7daf833014f83dc9929fc8fa78a5b0fd4
CastaChick <casta46chick@gmail.com>
2024-11-27 18:58:33 +0900

    【Build Failed】only change used params by running npx @next/codemod@canary upgrade latest

--- src/app/example-used-params/[id]/page.tsx
+++ src/app/example-used-params/[id]/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
 interface ExampleUsedParamsProps {
-   params: {
+   params: Promise<{
         id: string;
-   }
+   }>
 }
 
-export default function ExampleUsedParams({params}: ExampleUsedParamsProps) {
+export default async function ExampleUsedParams(props: ExampleUsedParamsProps) {
+  const params = await props.params;
   console.log(params.id)
-  
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
